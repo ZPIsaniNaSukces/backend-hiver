35be267204ea3a4fc9bec3e67eeb641d7d599241
Jakub Czajkowski <and.jj.an@gmail.com>
2025-10-09 18:47:22 +0200

    feat: let's get this party started

---
+++ apps/users/test/app.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { UsersModule } from './../src/users.module';
+
+describe('UsersController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    // Blogoslawienstw od Bartusia
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
