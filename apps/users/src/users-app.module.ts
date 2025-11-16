cb2ffbbe8c962acf0789fb1e4137ba31bf02bf77
Jakub Czajkowski <and.jj.an@gmail.com>
2025-11-16 01:06:15 +0100

    refactor(auth): use AuthClientModule to avoid cross db connections

--- apps/users/src/users-app.module.ts
+++ apps/users/src/users-app.module.ts
@@ -5,7 +5,6 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 import { CompaniesModule } from "./companies/companies.module";
-import { USERS_PRISMA, UsersPrismaClient } from "./prisma/prisma.constants";
 import { TeamsModule } from "./teams/teams.module";
 import { UsersAppController } from "./users-app.controller";
 import { UsersModule } from "./users/users.module";
@@ -13,12 +12,7 @@ import { UsersModule } from "./users/users.module";
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule.forRoot({
-      provide: USERS_PRISMA,
-      client: UsersPrismaClient,
-      databaseUrlEnv: "DATABASE_URL",
-      global: true,
-    }),
+    PrismaModule.forRoot({ global: true }),
     UsersModule,
     TeamsModule,
     CompaniesModule,
